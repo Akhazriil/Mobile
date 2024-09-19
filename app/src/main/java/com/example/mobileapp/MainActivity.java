b10c4590236c7b9e4f910b313cc68c40ab358e87
Akhazriil <kuzmin.reyno@mail.ru>
2024-09-19 14:27:46 +0300

    Сделал первую лабораторную

---
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mobileapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.lang.Object;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        };
+
+    public void Click(View view) {
+        Toast.makeText(this,"Привет", Toast.LENGTH_SHORT).show();
+    }
+}
